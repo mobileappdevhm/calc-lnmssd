d51ebc831b6348ed532ff1da6768987b885fa933
lnmssd <35195627+lnmssd@users.noreply.github.com>
2018-03-30 22:32:30 +0200

    readme updated

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -12,88 +12,188 @@ public class MainActivity extends AppCompatActivity {
 
     TextView result;
     EditText numberOne, numberTwo;
-    Button add, subtract, divide, multiply;
 
+    Button add, subtract, divide, multiply, dot, equal;
     Button one, two, three, four, five, six, seven, eight, nine, zero;
 
+    boolean ad, sub, div, mult;
 
-    float result_num;
+    double result_num;
 
-    int num1, num2;
+    double num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result = (TextView)findViewById(R.id.result);
-        numberOne = (EditText)findViewById(R.id.numberOne);
-        numberTwo = (EditText)findViewById(R.id.numberTwo);
-        add = (Button)findViewById(R.id.add);
-        subtract = (Button)findViewById(R.id.subtract);
-        divide = (Button)findViewById(R.id.divide);
-        multiply = (Button)findViewById(R.id.multiply);
 
+        result = (TextView) findViewById(R.id.result);
+        add = (Button) findViewById(R.id.add);
+        subtract = (Button) findViewById(R.id.subtract);
+        divide = (Button) findViewById(R.id.divide);
+        multiply = (Button) findViewById(R.id.multiply);
+        dot = (Button) findViewById(R.id.dot);
+        equal = (Button) findViewById(R.id.equals);
+
+        ////////////////////////////////////////////////////////
+
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "1");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "2");
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "3");
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "4");
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "5");
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "6");
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "7");
+            }
+        });
+
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "8");
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "9");
+            }
+        });
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + "0");
+            }
+        });
+
+
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(result.getText() + ".");
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                 num1 = Integer.parseInt(numberOne.getText().toString());
-                num2 = Integer.parseInt(numberTwo.getText().toString());
-
-                result_num = num1 + num2;
-                result.setText(String.valueOf(result_num));
-
-
+            public void onClick(View view) {
+                num1 = Double.parseDouble(result.getText() + "");
+                ad = true;
+                result.setText(null);
             }
-
         });
 
         subtract.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                num1 = Integer.parseInt(numberOne.getText().toString());
-                num2 = Integer.parseInt(numberTwo.getText().toString());
-
-                result_num = num1 - num2;
-                result.setText(String.valueOf(result_num));
-
-
+            public void onClick(View view) {
+                num1 = Double.parseDouble(result.getText() + "");
+                sub = true;
+                result.setText(null);
             }
         });
-
-        divide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                num1 = Integer.parseInt(numberOne.getText().toString());
-                num2 = Integer.parseInt(numberTwo.getText().toString());
-
-                String myToastText = "Durch 0 teilen nicht möglich!";
-
-                if (num2 == 0){
-                    Toast myErrorMsg = Toast.makeText(getApplicationContext(), myToastText, Toast.LENGTH_LONG);
-                    result.setText(String.valueOf("N/A"));
-                    myErrorMsg.show();
-                } else {
-                    result_num = num1 / num2;
-                    result.setText(String.valueOf(result_num));
-                }
-
-            }
-        });
-
 
         multiply.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                num1 = Integer.parseInt(numberOne.getText().toString());
-                num2 = Integer.parseInt(numberTwo.getText().toString());
+            public void onClick(View view) {
+                num1 = Double.parseDouble(result.getText() + "");
+                mult = true;
+                result.setText(null);
+            }
+        });
 
-                result_num = num1 * num2;
-                result.setText(String.valueOf(result_num));
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(result.getText() + "");
+                div = true;
+                result.setText(null);
+            }
+        });
+
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num2 = Double.parseDouble(result.getText() + "");
+                if (ad == true) {
+                    result.setText(num1 + num2 + "");
+                    ad = false;
+                }
+                if (sub == true) {
+                    result.setText(num1 - num2 + "");
+                    sub = false;
+
+                }
+                if (mult == true) {
+                    result.setText(num1 * num2 + "");
+                    mult = false;
+                }
+                if (div == true) {
+                    result.setText(num1 / num2 + "");
+                    div = false;
+                }
+
 
             }
         });
 
-    }
 
+
+
+
+    }
 }
